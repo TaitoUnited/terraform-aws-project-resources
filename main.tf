5635284ca2ca062adf7940375a5cfa5d4ef42bbb
keskiju <jukka.keskiluopa@taitounited.fi>
2021-04-08 21:21:50 +0300

    fix: remove invalid coalesce rule

--- main.tf
+++ main.tf
@@ -175,7 +175,7 @@ locals {
   gatewayFunctionsById = {
     for name, service in local.servicesById:
     name => service
-    if var.create_ingress && local.ingress.enabled && service.type == "function" && coalesce(service.path, "") != ""
+    if var.create_ingress && local.ingress.enabled && service.type == "function" && service.path != null
   }
 
   gatewayStaticContentsById = {
